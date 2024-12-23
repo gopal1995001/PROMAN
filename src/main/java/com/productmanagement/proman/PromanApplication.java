084968add48569361f80bc40daac4fffd47ef37f
Rahul <dasr53357@gmail.com>
2024-12-23 19:37:25 +0530

    Second Collab

--- src/main/java/com/productmanagement/proman/PromanApplication.java
+++ src/main/java/com/productmanagement/proman/PromanApplication.java
@@ -9,6 +9,7 @@ public class PromanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PromanApplication.class, args);
 		System.out.println("New Changes for Collaboration.");
+		System.out.println("New Changes for Collaboration.");
 	}
 
 }
