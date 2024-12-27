94bf933dbad028161c7479eb0119080eeec7a13a
Rahul <dasr53357@gmail.com>
2024-12-27 11:15:37 +0530

    jksjd

--- src/main/java/com/productmanagement/proman/corsconfig/WebConfig.java
+++ src/main/java/com/productmanagement/proman/corsconfig/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://e-commerce-project-lovat-two.vercel.app","https://ecommerce-goap.vercel.app","http://localhost:5173/")
+                        .allowedOrigins("https://e-commerce-project-lovat-two.vercel.app","https://ecommerce-goap.vercel.app","http://localhost:5173","http://localhost:5174")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
